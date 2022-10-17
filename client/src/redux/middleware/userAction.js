587e06db82c846bf63cc1b810d61b373d6d25fa7
nataya1116 <nataya1116@gmail.com>
2022-10-17 11:17:08 +0900

    모델 폴더 중 하나 삭제

--- client/src/redux/middleware/userAction.js
+++ client/src/redux/middleware/userAction.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-function login(id, pw, setWarp){
+function login(id, pw){
     return async(dispatch, getState) => {
         const url = "http://localhost:8000/login";
         const user = await axios({
@@ -14,7 +14,7 @@ function login(id, pw, setWarp){
         console.log(user);
         alert(user.data);
         if(user.data === "로그인 성공") {
-            setWarp();
+
             dispatch({type : "LOG_IN", payload:{id, pw}})
         }else{
             alert("로그인 실패")
